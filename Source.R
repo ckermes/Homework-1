3b7969511ebe1fec35a67d3436a356295cc890a6
ckermes <ckermes2@gmail.com>
2020-04-14 22:15:25 -0400

    works

--- Source.R
+++ Source.R
@@ -1,38 +1,32 @@
-#reads data into the environment
-library(data.table)
-files <- list.files('specdata',pattern = ".csv", full.names = TRUE) #creates a list of full names and where data is sourcing from
-temp <- lapply(files, read.csv, sep = ",")  #creates a holder with all of the individual .csv files
-data <- rbind( temp ) #rbinds all the .csv files together into one file
-
-meanfunc <- function()
-  
-
-    dat_subset <- data[which(data[pollutant,] == pollutant)]
 
 
-pollutantmean <- function(directory, pollutant, id = 1:332){
+pollutantmean <- function(directory, pollutant = "sulfate", id = 1:332){
   #creates a list of full names and where data is sourcing from
-   files <- list.files('specdata',pattern = ".csv", full.names = TRUE) 
+   dirlist <- list.files('specdata',pattern = ".csv", full.names = TRUE) 
    #creates a holder with all of the individual .csv files
-    temp <- lapply(files, read.csv, sep = ",") 
-    unlist(temp)
+    filenames <- dirlist[id]
+    #unlist(temp)
+    csvDataList <- vector(mode = 'list', length = length(filenames))
+    
+    csvDataList <- lapply(filenames, read.csv)
+    
+    obsDataFrame <- data.frame()
+    obsDataFrame <- do.call(rbind,csvDataList)
     #rbinds all the .csv files together into one file
-    data <- rbind( temp )
-    #subsets data into the right variable
-    for(i in id){
-      data1<-data[,pollutant]
-      
-      data2<-data1[!is.na(data1)]
+    
+    if (pollutant == 'sulfate'){
+        mymean <- mean(obsDataFrame$sulfate, na.rm = TRUE)
+    } else{
+        mymean <- mean(obsDataFrame$nitrate, na.rm = TRUE)
     }
-  
     
-    #creates median with no na values
-    median(data2$pollutant)
-    
+    return(mymean)
+
   }
 
 
-pollutantmean(data, sulphate, id)
+pollutantmean(specdata)
+
 
 
 
